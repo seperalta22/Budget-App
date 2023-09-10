94c614d618884db7afc7a7a0fd48791e872ef3de
Sergio Peralta <seperalta88@gmail.com>
2023-09-10 14:03:08 -0300

    Set up splash page and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  authenticated :user do
+    root to: 'categories#index', as: :authenticated_root
+  end
+  
   root to: "home#index"
+  devise_for :users
+  resources :home, only: [:index]
+  resources :categories, only: [:index]
 end
